4c6900ef8d771d7a56e8c6dc857e6fca5303c423
mpepe16 <mpepe66@gmail.com>
2022-09-22 20:07:53 +0200

    Add: promise exampl.

--- features/promises.ts
+++ features/promises.ts
@@ -2,12 +2,16 @@ function ExpensiveWebCall(time : number) : Promise<void> {
     return new Promise((resolve, reject) => setTimeout(resolve, time));
    }
    class MyWebService {
-    CallExpensiveWebOperation() : void {
-    ExpensiveWebCall(4000).then(()=> console.log(`Finished web
-    service`))
-    .catch(()=> console.log(`Expensive web call failure`));
+    async CallExpensiveWebOperation() {
+    try {
+    await ExpensiveWebCall(4000);
+    console.log(`Finished web service`);
+} catch (error) {
+    console.log(`Caught ${error}`);
+    }
     }
    }
+   
 
 console.log(`calling service`);
 new MyWebService().CallExpensiveWebOperation();
