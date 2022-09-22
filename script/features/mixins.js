81857293ecd5c8103e8cb5dbc3d0afecb9f841b2
mpepe16 <mpepe66@gmail.com>
2022-09-22 23:08:29 +0200

    Add: starter html and markdownparser ts.

---
+++ script/features/mixins.js
@@ -0,0 +1,33 @@
+"use strict";
+function RecordStatus(base) {
+    return class extends base {
+        constructor() {
+            super(...arguments);
+            this.deleted = false;
+        }
+        get Deleted() {
+            return this.deleted;
+        }
+        Delete() {
+            this.deleted = true;
+            console.log(`The record has been marked as deleted.`);
+        }
+    };
+}
+function Timestamp(base) {
+    return class extends base {
+        constructor() {
+            super(...arguments);
+            this.Updated = new Date();
+        }
+    };
+}
+class Person {
+    constructor(firstName, lastName) {
+        this.FirstName = firstName;
+        this.LastName = lastName;
+    }
+}
+const ActivePerson = RecordStatus(Timestamp(Person));
+let activePerson = new ActivePerson("Peter", "O'Hanlon");
+//# sourceMappingURL=mixins.js.map
---
+++ script/features/promises.js
@@ -0,0 +1,30 @@
+"use strict";
+var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
+    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
+    return new (P || (P = Promise))(function (resolve, reject) {
+        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
+        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
+        function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
+        step((generator = generator.apply(thisArg, _arguments || [])).next());
+    });
+};
+function ExpensiveWebCall(time) {
+    return new Promise((resolve, reject) => setTimeout(resolve, time));
+}
+class MyWebService {
+    CallExpensiveWebOperation() {
+        return __awaiter(this, void 0, void 0, function* () {
+            try {
+                yield ExpensiveWebCall(4000);
+                console.log(`Finished web service`);
+            }
+            catch (error) {
+                console.log(`Caught ${error}`);
+            }
+        });
+    }
+}
+console.log(`calling service`);
+new MyWebService().CallExpensiveWebOperation();
+console.log(`Processing continues until the web service returns`);
+//# sourceMappingURL=promises.js.map
